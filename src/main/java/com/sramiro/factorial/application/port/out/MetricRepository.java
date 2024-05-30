d02c865dc4e379cce7412e61096d723b3e581b9f
sramiro <sergio.ramiro87@gmail.com>
2024-05-30 17:35:43 +0200

    Refactored MetricRepository para extender de jpaRepository.

--- src/main/java/com/sramiro/factorial/application/port/out/MetricRepository.java
+++ src/main/java/com/sramiro/factorial/application/port/out/MetricRepository.java
@@ -1,8 +1,9 @@
 package com.sramiro.factorial.application.port.out;
 
 import com.sramiro.factorial.domain.model.Metric;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MetricRepository {
-    Metric save(Metric metric);
+public interface MetricRepository extends JpaRepository<Metric, Long> {
+//    Metric save(Metric metric);
 
 }
