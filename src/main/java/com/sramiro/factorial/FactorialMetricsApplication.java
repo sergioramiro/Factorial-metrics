162be28768a3246dc45bad2b6800f21c9621f17a
sramiro <sergio.ramiro87@gmail.com>
2024-05-30 11:42:46 +0200

    First commit

---
+++ src/main/java/com/sramiro/factorial/FactorialMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.sramiro.factorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sramiro/factorial/FactorialMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sramiro.factorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
