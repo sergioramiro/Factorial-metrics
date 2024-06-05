1a55ba3a9a9ef3e26efb53fd8ea0ab1d239cb0fb
sramiro <sergio.ramiro87@gmail.com>
2024-06-05 11:34:33 +0200

    Fixed CORS issue

--- backend/src/main/java/com/sramiro/factorial/infraestructure/rest/spec/MetricControllerSpec.java
+++ backend/src/main/java/com/sramiro/factorial/infraestructure/rest/spec/MetricControllerSpec.java
@@ -6,6 +6,7 @@ import com.sramiro.factorial.infraestructure.rest.api.dto.response.MetricRespons
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,7 @@ public interface MetricControllerSpec {
     );
 
     @GetMapping("/names")
+    @CrossOrigin(origins = "http://localhost:3000")
     @ResponseStatus(HttpStatus.OK)
     List<String> getAllMetricNames();
 }
