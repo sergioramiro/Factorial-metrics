296d07d407444a1363ee02527b02225cf11ba741
sramiro <sergio.ramiro87@gmail.com>
2024-06-06 13:51:25 +0200

    Implemented getMetricsEndpoint_ShouldReturnTenMetrics in MetricsEndpointIT

--- backend/src/test/java/com/sramiro/factorial/integration/MetricsEndpointIT.java
+++ backend/src/test/java/com/sramiro/factorial/integration/MetricsEndpointIT.java
@@ -2,10 +2,10 @@ package com.sramiro.factorial.integration;
 
 import com.sramiro.factorial.application.port.out.MetricRepository;
 import com.sramiro.factorial.configuration.TestContainerConfiguration;
+import com.sramiro.factorial.domain.model.Metric;
 import com.sramiro.factorial.infraestructure.rest.api.dto.request.CreateMetricRequest;
 import io.restassured.RestAssured;
-import lombok.SneakyThrows;
-import org.junit.jupiter.api.Assertions;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +13,13 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MetricsEndpointIT extends TestContainerConfiguration {
+    public static final String METRICS = "/metrics";
+    public static final String TEST = "test";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final double VALUE = 15.0;
     @Autowired
     private MetricRepository metricRepository;
 
@@ -24,22 +29,39 @@ class MetricsEndpointIT extends TestContainerConfiguration {
     @BeforeEach
     void before() {
         RestAssured.port = port;
+        metricRepository.deleteAll();
     }
 
     @Test
-    @SneakyThrows
-    void test() {
+    void postMetricEndpoint_ShouldIncreaseMetricCountInDatabase() {
         long count = metricRepository.count();
         given()
-                .contentType("application/json")
-                .body(CreateMetricRequest.builder().name("test").value(15.0).build())
+                .contentType(CONTENT_TYPE)
+                .body(CreateMetricRequest.builder().name(TEST).value(VALUE).build())
                 .when()
-                .post("/metrics")
+                .post(METRICS)
                 .then()
                 .statusCode(HttpStatus.CREATED.value());
         long count2 = metricRepository.count();
-        Assertions.assertEquals(count + 1, count2);
+        assertEquals(count + 1, count2);
     }
 
+    @Test
+    void getMetricsEndpoint_ShouldReturnTenMetrics() {
+        for (int i = 0; i < 10; i++) {
+            metricRepository.save(Metric.builder().value(VALUE).name(TEST).build());
+        }
+        Response response = given()
+                .get(METRICS)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract().response();
+
+        int metricCount = response.jsonPath().getList("$").size();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+        assertEquals(10, metricCount);
+
+    }
 
 }
