3825581689209513d121f13eb0805c417fec70cf
sramiro <sergio.ramiro87@gmail.com>
2024-06-08 17:03:35 +0200

    Configured Flyway. Added config class and the sql scripts

---
+++ backend/src/main/java/com/sramiro/factorial/infraestructure/db/configuration/flyway/FlywayConfig.java
@@ -0,0 +1,32 @@
+package com.sramiro.factorial.infraestructure.db.configuration.flyway;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public Flyway flyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(url, username, password)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+}
